096d13528161cedf660b1b0de2a62933afeeb777
sanjaypanchal61 <sanjaypanchal6122@gmail.com>
2026-03-13 23:54:56 +0530

    learn string

---
+++ 4_string/README.md
@@ -0,0 +1,15 @@
+## Day 4 - Strings
+
+### Definition
+
+A string is a sequence of characters enclosed within single quotes (' ') or double quotes (" ").
+
+---
+
+### Important Points
+
+* Strings are used to store **text data**.
+* Strings can be written using **single quotes (' ')** or **double quotes (" ")**.
+* Strings are **immutable**, which means they cannot be changed after creation.
+* Each character in a string has an **index number**.
+* Indexing in Python starts from **0**.
---
+++ 4_string/string.py
@@ -0,0 +1,36 @@
+# string example
+name = "aman"
+city = 'pune'
+
+print(name)
+print(city)
+
+
+# multi-line string
+text = """example of multi-line
+string"""
+print(text)
+
+# string indexing
+language = "python"
+print(language[0])
+print(language[3])
+
+# string slicing
+text = "hello world"
+print(text[0:5])
+
+# string concatenation
+first = "hello"
+second = "world"
+
+result = first + " " + second
+print(result)
+
+# string methods
+text = "hello world"
+
+print(text.replace("world", "python")) #string replace
+print(text.upper()) #string upper case
+print(text.lower()) #string lower case
+print(len(text))#string length
