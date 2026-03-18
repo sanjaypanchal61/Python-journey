088889727eed8d9766839b4cb3ce96f7aca31561
sanjaypanchal61 <sanjaypanchal6122@gmail.com>
2026-03-18 20:32:37 +0530

    learn list

---
+++ 9_list/README.md
@@ -0,0 +1,22 @@
+# Day 9 – List
+
+## 📖 Definition
+
+A **list** is a collection of items that are ordered, changeable, and allow duplicate values.
+
+---
+
+## 💻 Creating List
+
+```python
+fruits = ["apple", "banana", "mango"]
+```
+
+---
+
+## Important Points
+
+* Lists are ordered
+* Lists are changeable
+* Allows duplicate values
+* Can store multiple data types
---
+++ 9_list/list.py
@@ -0,0 +1,44 @@
+# example
+fruits = ["apple", "banana", "mango"]
+
+print(fruits)
+
+
+# indexing
+fruits = ["apple", "banana", "mango"]
+
+print(fruits[0])
+print(fruits[1])
+
+
+# list slicing
+print(fruits[0:2])
+
+# list modify
+fruits[1] = "orange"
+print(fruits)
+
+
+# list methods
+
+# append()
+fruits.append("grapes")
+
+# insert()
+fruits.insert(1,"kiwi")
+
+# remove()
+fruits.remove("apple")
+
+# pop()
+fruits.pop()
+
+# sort()
+num = [6, 3, 10, 2]
+num.sort()
+
+# reverse()
+num.reverse()
+
+# length
+print(len(num))
