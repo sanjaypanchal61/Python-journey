4a75ae8da51910bb0f09f88ea1511e916156769d
sanjaypanchal61 <sanjaypanchal6122@gmail.com>
2026-03-21 23:09:27 +0530

    learn tuple

---
+++ 10_tuples/README.md
@@ -0,0 +1,13 @@
+# Day 10 – Tuples
+
+## 📖 Definition
+
+A **tuple** is a collection of items that are ordered and unchangeable (immutable).
+
+---
+
+## ⭐ Important Points
+
+* Tuples are immutable
+* Faster than lists
+* Allows duplicate values
---
+++ 10_tuples/tuples.py
@@ -0,0 +1,27 @@
+# create tuple
+
+fruits = ("apple", "banana", "mango")
+print(fruits)
+
+
+# indexing
+fruits = ("apple", "banana", "mango")
+
+print(fruits[0])
+print(fruits[1])
+
+# length
+print(len(fruits))
+
+# slicing
+print(fruits[0:2])
+
+
+# tuple methods
+
+# count()
+numbers = (1, 2, 2, 3)
+print(numbers.count(2))
+
+# index()
+print(numbers.index(3))
